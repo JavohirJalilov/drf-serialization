7e26e39c2ab0b4121c0cff72beb51c648bb308c7
JavohirJalilov <javohirjaliloff@gmail.com>
2023-04-08 10:09:15 +0500

    rest_fremwork

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+# APPEND SLASH Django setting
+# https://docs.djangoproject.com/en/4.0/ref/settings/#append-slash
+APPEND_SLASH = True
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
